32f3b43df3d1c9124e6b41e50327d3b7d66fb58c
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-02-11 14:18:46 -0300

    Class11, removeElements

---
+++ class10-removeElements/removeElements.js
@@ -0,0 +1,7 @@
+const contentArea = document.getElementById('listArea')
+const contentArea2 = document.getElementById('listArea2')
+const prependButton = document.getElementById('prepend');
+const appendButton = document.getElementById('append');
+const beforeButton = document.getElementById('before');
+const afterButton = document.getElementById('after');
+
