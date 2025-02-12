3eab7187283ee1df108f5a9ceb20510e20291ca5
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-02-11 22:26:00 -0300

    Class 12, clone method.

---
+++ class11-cloneAndReplace/cloneReplace.js
@@ -0,0 +1,4 @@
+const contentArea = document.getElementById('contentArea');
+const uL = document.getElementById('listArea');
+const pToClone = contentArea.querySelector('p');
+const body = document.querySelector('body');
