09f3bf761520cc63e6f288869372ccfae03bad18
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-02-18 08:23:23 -0300

    Developing the function that creates a new list item after submiting the form.

--- class16-creatingATaskList/app.js
+++ class16-creatingATaskList/app.js
@@ -6,5 +6,14 @@ taskForm.addEventListener('submit', (event) => {
   event.preventDefault()
   const taskInput = document.getElementById('task-input');
   const task = taskInput.value
-  console.log(task)
+  const liItem = document.createElement('li');
+  const deleteBtn = document.createElement('span')
+  deleteBtn.classList.add('delete-btn')
+  deleteBtn.textContent = '❌'
+  const editBtn = document.createElement('span')
+  editBtn.classList.add('edit-btn')
+  editBtn.textContent = '✏️'
+  liItem.textContent = task;
+  liItem.append(deleteBtn, editBtn)
+  taskList.append(liItem)
 })
