f4f240c53e913246a440f6c7f05a9212c3d1b291
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-02-19 15:51:26 -0300

    Going back to previous version

--- class16-creatingATaskList/app.js
+++ class16-creatingATaskList/app.js
@@ -1,8 +1,8 @@
 const taskForm = document.getElementById('task-form');
-
 const taskList = document.getElementById('task-list');
 
-loadTasksInLocalStorage()
+
+
 
 function createTask(task) {
   const liItem = document.createElement('li');
@@ -36,15 +36,16 @@ function saveTasksInLocalStorage(task) {
 }
 
 function loadTasksInLocalStorage(){ 
-  const taskList = JSON.parse(localStorage.getItem('tasks')) || []
-  taskList.forEach((task) => {
+  taskList.textContent = ""
+  const taskListUpdated = JSON.parse(localStorage.getItem('tasks')) || []
+  taskListUpdated.forEach((task) => {
     createTask(task)
   })
 }
 
 function updateLocalStorage() {
   const tasks = Array.from(taskList.querySelectorAll('li'))
-  const listUpdated = tasks.map((task) => task.firstChild.textContent)
+  const listUpdated = tasks.map((task) => task.firstChild.textContent.trim())
   localStorage.setItem('tasks', JSON.stringify(listUpdated))
 }
 
@@ -53,27 +54,40 @@ function deleteTask(task){
 }
 
 function editTask(task) {
-  const newTask = prompt('Edit task', task.firstChild.textContent)
+  const oldTask = task.firstChild.textContent.trim()
+  const newTask = prompt('Edit task', oldTask)
   if(newTask) {
     task.firstChild.textContent = newTask
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || []
+    const index = tasks.indexOf(oldTask)
+    if (index !== -1) {
+      tasks[index] = newTask
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks))
   }
 }
 
-taskForm.addEventListener('submit', (event) => {
-  event.preventDefault()
-  const taskInput = document.getElementById('task-input');
-  const task = taskInput.value
-  createTask(task)
-  saveTasksInLocalStorage(task)
-})
 
-taskList.addEventListener('click', (event) => {
-  if(event.target.classList.contains('delete-btn')) {
-    saveDeletedTasksInLocalStorage(event.target.parentElement)
-    deleteElementFromLocalStorage(event.target.parentElement)
-    deleteTask(event.target.parentElement)
-  } else if (event.target.classList.contains('edit-btn')) {
-    editTask(event.target.parentElement)
-    updateLocalStorage()
-  }
-})
+document.addEventListener('DOMContentLoaded', () => {
+  taskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const taskInput = document.getElementById('task-input');
+    const task = taskInput.value
+    createTask(task)
+    saveTasksInLocalStorage(task)
+  })
+  
+  taskList.addEventListener('click', (event) => {
+    if(event.target.classList.contains('delete-btn')) {
+      saveDeletedTasksInLocalStorage(event.target.parentElement)
+      deleteElementFromLocalStorage(event.target.parentElement)
+      deleteTask(event.target.parentElement)
+    } else if (event.target.classList.contains('edit-btn')) {
+      editTask(event.target.parentElement)
+      updateLocalStorage()
+    }
+  })
+  loadTasksInLocalStorage();
+});
+
+
